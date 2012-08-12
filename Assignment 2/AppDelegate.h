ef4832c57d47103c8ffa91ccaaa6990f72c9adba
Karim Ainine <karim.ainine@gmail.com>
2012-08-12 19:43:19 +1000

    Initial Commit

---
+++ Assignment 2/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  Assignment 2
+//
+//  Created by Karim Ainine on 12/08/12.
+//  Copyright (c) 2012 Karim Ainine. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
---
+++ Assignment 2/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Assignment 2
+//
+//  Created by Karim Ainine on 12/08/12.
+//  Copyright (c) 2012 Karim Ainine. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
